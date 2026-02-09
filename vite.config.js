29b813f22c1e8da3aca2a2e5182160dcc09ae12e
Gurudatt <106651520+Gurudatt-Goudar@users.noreply.github.com>
2026-02-09 19:14:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Gurudattportfolio/', // Make sure this matches your GitHub repository name
+  base: '/Gurudattportfolio/',
 })
